df79364e550fd232d5aa9ecaeab00232a9657f7d
Sanskar Sharma <sanskarfc@gmail.com>
2023-08-20 11:47:59 +0530

    Added Info bubble beside text box in filter options

--- components/FilterOptions.jsx
+++ components/FilterOptions.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 
 const FilterOptions = () => {
   const [selectedLadies, setSelectedLadies] = useState('1');
   const [selectedMen, setSelectedMen] = useState('1');
   const [detourMinutes, setDetourMinutes] = useState('');
+  const [showDetourInfo, setShowDetourInfo] = useState(false);
 
   const ladiesOptions = ['1', '2', '3', '4'];
   const menOptions = ['1', '2', '3', '4'];
 
+  const toggleDetourInfo = () => {
+    setShowDetourInfo(!showDetourInfo);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Preferences</Text>
+      <Text style={styles.heading}>Filter Options</Text>
       <View style={styles.filterContainer}>
         <Text style={styles.filterLabel}>Number of Ladies in the Car:</Text>
         {ladiesOptions.map(option => (
@@ -43,13 +48,26 @@ const FilterOptions = () => {
       </View>
       <View style={styles.filterContainer}>
         <Text style={styles.filterLabel}>Number of Minutes of Detour:</Text>
-        <TextInput
-          style={styles.input}
-          value={detourMinutes}
-          onChangeText={text => setDetourMinutes(text)}
-          keyboardType="numeric"
-          placeholder="Enter minutes"
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={detourMinutes}
+            onChangeText={text => setDetourMinutes(text)}
+            keyboardType="numeric"
+            placeholder="Enter minutes"
+          />
+          <TouchableOpacity onPress={toggleDetourInfo}>
+            <Text style={styles.infoButton}>ℹ️</Text>
+          </TouchableOpacity>
+        </View>
+        {showDetourInfo && (
+          <View style={styles.infoBubble}>
+            <Text style={styles.infoText}>
+              Enter the number of minutes of detour that is acceptable to you
+              so that you can drop off your fellow rider to their destination.
+            </Text>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -89,6 +107,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 5,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   input: {
     width: 120,
     height: 40,
@@ -96,6 +118,21 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingHorizontal: 10,
     borderRadius: 10,
+  },
+  infoButton: {
+    fontSize: 20,
+    marginLeft: 10,
+  },
+  infoBubble: {
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  infoText: {
+    fontSize: 14,
   },
 });
 

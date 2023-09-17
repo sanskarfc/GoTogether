7197e23df7fdac466752258032ca9c8f895440b0
Sanskar Sharma <sanskarfc@gmail.com>
2023-09-17 15:24:58 +0530

    Changed SQL Schemas for planetscale

--- gtdatabase/gtdb.sql
+++ gtdatabase/gtdb.sql
@@ -2,7 +2,7 @@ DROP database if exists gtdatabase;
 create database gtdatabase;
 use gtdatabase;
 
-Drop table if exists users;
+Drop table if exists Users;
 CREATE TABLE Users (
     user_id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE Users (
     ratings DECIMAL(3, 2) CHECK (ratings >= 0 AND ratings <= 5)
 );
 
-drop table if exists trip;
+drop table if exists Trip;
 CREATE TABLE Trip (
     trip_id INT PRIMARY KEY,
     start_altitude DECIMAL(10, 2) NOT NULL,
@@ -22,73 +22,72 @@ CREATE TABLE Trip (
     start_time TIMESTAMP NOT NULL,
     number_of_seats INT NOT NULL,
     rideby INT NOT NULL,
-    FOREIGN KEY (rideby) REFERENCES Users(user_id)
+    KEY rideby_idx (rideby)
 );
 
-drop table if exists tripusermatch;
+drop table if exists TripUserMatch;
 CREATE TABLE TripUserMatch (
     match_id INT PRIMARY KEY,
     trip_id INT,
     user_id INT,
-    FOREIGN KEY (trip_id) REFERENCES Trip(trip_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    KEY trip_id_idx (trip_id),
+    KEY user_id_idx (user_id)
 );
 
 
 
-drop table if exists personalchat;
+drop table if exists PersonalChat;
 CREATE TABLE PersonalChat (
     chat_id INT PRIMARY KEY,
     sender INT NOT NULL,
     receiver INT NOT NULL,
-    FOREIGN KEY (sender) REFERENCES Users(user_id),
-    FOREIGN KEY (receiver) REFERENCES Users(user_id)
+    KEY sender_idx (sender),
+    KEY receiver_idx (receiver)
 );
 
-drop table if exists message;
+drop table if exists Message;
 CREATE TABLE Message (
     message_id INT PRIMARY KEY,
     message_text TEXT NOT NULL,
     message_number INT NOT NULL
 );
 
-drop table if exists chatmessage;
+drop table if exists ChatMessage;
 CREATE TABLE ChatMessage (
     chat_message_id INT PRIMARY KEY,
     message_id INT,
     personal_chat_id INT,
     time TIMESTAMP NOT NULL,
-    FOREIGN KEY (message_id) REFERENCES Message(message_id),
-    FOREIGN KEY (personal_chat_id) REFERENCES PersonalChat(chat_id)
+    KEY message_id_idx (message_id),
+    KEY personal_chat_id_idx (personal_chat_id)
 );
 
 
-drop table if exists grouptable;
+drop table if exists GroupTable;
 CREATE TABLE GroupTable (
     group_id INT PRIMARY KEY,
     group_name VARCHAR(255) NOT NULL,
     group_created_time TIMESTAMP NOT NULL,
     created_by INT NOT NULL,
-    FOREIGN KEY (created_by) REFERENCES Users(user_id)
+    KEY created_by_idx (created_by)
 );
 
-drop table if exists groupmembers;
+drop table if exists GroupMembers;
 CREATE TABLE GroupMembers (
     member_id INT PRIMARY KEY,
     join_time TIMESTAMP NOT NULL,
     user_id INT NOT NULL,
-    
     group_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (group_id) REFERENCES GroupTable(group_id)
+    KEY user_id_idx (user_id),
+    KEY group_id_idx (group_id)
 );
 
-drop table if exists grpchat;
+drop table if exists GrpChat;
 CREATE TABLE GrpChat (
     grp_chat_id INT PRIMARY KEY,
     group_id INT,
     message_id INT,
     time TIMESTAMP NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES GroupTable(group_id),
-    FOREIGN KEY (message_id) REFERENCES Message(message_id)
+    KEY group_id_idx (group_id),
+    KEY message_id_idx (message_id)
 );

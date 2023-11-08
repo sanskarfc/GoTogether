c0c35df2aac3cab33d6f59920b8a08ebea2a1e5f
MihirSutariya <sutariya.mihirkumar@iitgn.ac.in>
2023-11-08 19:08:37 +0530

    Changes Signin Page UI

--- components/SignInWithOAuth.jsx
+++ components/SignInWithOAuth.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import * as WebBrowser from "expo-web-browser";
-import { Button, View } from "react-native";
+import { View, Image, Text, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
 import { useOAuth } from "@clerk/clerk-expo";
 import { useWarmUpBrowser } from "../hooks/useWarmUpBrowser";
 
 WebBrowser.maybeCompleteAuthSession();
 
+
 const SignInWithOAuth = () => {
   useWarmUpBrowser();
-
   const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
 
   const onPress = React.useCallback(async () => {
@@ -16,8 +16,7 @@ const SignInWithOAuth = () => {
       const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow();
       if (createdSessionId) {
         setActive({ session: createdSessionId });
-      }
-      else {
+      } else {
         // Use signIn or signUp for next steps such as MFA
       }
     } catch (err) {
@@ -26,14 +25,55 @@ const SignInWithOAuth = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.container}>
+        <Image source={require('./img/logo.png')} style={styles.logo}  resizeMode="contain" />
 
-    <Button
-      title="Sign in with Google"
-      onPress={onPress}
-      />
-      
-    </View>
+        <TouchableOpacity style={styles.signInButton} onPress={onPress} >
+          <View style={styles.buttonContent}>
+            <Image source={require('./img/glogo.png')} style={styles.googleLogo} />
+            <Text style={styles.buttonText}>Sign in with Google</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  logo: {
+    width: '85%',
+    height: 'auto',
+    aspectRatio: 1, 
+  },
+  signInButton: {
+    backgroundColor: 'white',
+    width: '75%',
+    height: '5%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    bottom: 60,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    
+  },
+  googleLogo: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 18,
+  },
+});
+
+
 export default SignInWithOAuth;
